70bcbfdecadcf3d06672f2db4c340d54e12d7fb5
allenjstewart <allen.j.stwrt@gmail.com>
2019-04-07 17:15:13 -0700

    Disctionary for scheduling purposes

---
+++ Schedule_Dictionary.py
@@ -0,0 +1,33 @@
+allenjstewartSchedule={'Monday':{'12PM-3PM':True},'Tuesday':{'10:30AM-12:30PM':True},'Wednesday':{'11AM-12PM':True},'Thursday':{'10:30AM-12PM':True,'2PM-3PM':True}}
+christinelindcoleSchedule={}
+stevenrkleeSchedule={}
+brianfischerSchedule={}
+bergeyheatherSchedule={}
+CarterJohnSchedule={}
+allisonhenrichSchedule={}
+sukishergillSchedule={}
+asipleSchedule={}
+korrubiSchedule={}
+hannahpotgieterSchedule={}
+trettonjSchedule={}
+HbeuthelSchedule={}
+cabezacSchedule={}
+ZimcratSchedule={}
+TemplarghostSchedule={}
+
+
+MasterSchedule={'allenjstewart':allenjstewartSchedule,'christinelindcole':christinelindcoleSchedule,
+'stevenrklee':stevenrkleeSchedule, 'brianfischer':
+brianfischerSchedule, 'bergeyheather':
+bergeyheatherSchedule,'CarterJohn':
+CarterJohnSchedule, 'allisonhenrich':
+allisonhenrichSchedule,'sukishergill':
+sukishergillSchedule,'asiple':
+asipleSchedule,'korrubi':
+korrubiSchedule,'hannahpotgieter':
+hannahpotgieterSchedule,'trettonj':
+trettonjSchedule,'Hbeuthel':
+HbeuthelSchedule,'cabezac':
+cabezacSchedule,'Zimcrat':
+ZimcratSchedule,'Templarghost':
+TemplarghostSchedule}
