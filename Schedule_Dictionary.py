457619bd1ec8de729727a5184f79c68279d0e4dd
Brian Fischer <30986769+brian-fischer@users.noreply.github.com>
2019-04-09 09:10:00 -0700

    Update Schedule_Dictionary.py

--- Schedule_Dictionary.py
+++ Schedule_Dictionary.py
@@ -1,7 +1,7 @@
 allenjstewartSchedule={'Monday':{'12PM-3PM':True},'Tuesday':{'10:30AM-12:30PM':True},'Wednesday':{'11AM-12PM':True},'Thursday':{'10:30AM-12PM':True,'2PM-3PM':True}}
 christinelindcoleSchedule={'Monday':{'10:00AM-11:30AM':True},'Tuesday':{'3:40PM-5:00PM':True},'Wednesday':{'10:00AM-11:30AM':True},'Thursday':{'11:30AM-12:30PM':True},'Friday':{'10:00AM-11:30AM':True, '3:45PM-5:00PM':True}}
 stevenrkleeSchedule={}
-brianfischerSchedule={}
+brianfischerSchedule={'Monday':{'9AM-12PM':True},'Tuesday':{'9AM-12PM':True},'Thursday':{'9AM-12PM':True},'Friday':{'9AM-12PM':True}}
 bergeyheatherSchedule={'Monday':{'9AM-12:30PM':True,'2PM-4:30PM':True},'Tuesday':{'12:30PM-1:30PM':True},'Wednesday':{'2PM-4:30PM':True},'Thursday':{'12:30PM-1:30PM':True},'Friday':{'2PM-4:30PM':True}}
 CarterJohnSchedule={}
 allisonhenrichSchedule={'Monday':{'9AM-12PM':True},'Tuesday':{'10:00AM-3:00PM':True},'Wednesday':{'11AM-12PM':True},'Thursday':{'12:30PM-1:30PM':True},'Friday':{'11AM-12PM':True}}
